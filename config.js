634b789217c1146f770f7d4875a52c76c541e4e0
DaniBotz <108689250+YTDANIBOTZ@users.noreply.github.com>
2022-07-23 15:30:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ global.prems = ['6281251549899'] //ganti pakai nomormu biar bot berfungsi
 //=============『 Info Owner 』============== //
 global.nameowner = 'ᴹᴿ᭄ DaniBotzOffcོ_1ঔৣ꧂'
 global.numberowner = '6281251549899' //ganti pakai nomor mu
-global.instagram = 'Gk Ad Bwang:v' //ganti pakai Instagram mu
+global.instagram = 'https://www.instagram.com/danibotz_official' //ganti pakai Instagram mu
 global.github = 'https://github.com/YTDANIBOTZ' //ganti aja pakai githubmu
 global.tt = '"_"' //ganti aja pakai tiktok mu
 global.sc = 'https://youtu.be/lMcwJMp_0rM' //jngn ganti hargai creator
@@ -27,7 +27,7 @@ global.gopay = '081352848936' //ganti pakai nomor Gopay mu
 global.namebot = '❋ཻུ۪۪⸙Danibotz-Ofc⳹ ❋ཻུ۪۪' //ganti jadiin nama bot mu
 global.gc = 'https://chat.whatsapp.com/BiJs5Xl4oJA1Ama5BahUwd' //ganti pakai grup mu
 global.web = 'https://youtube.com/channel/UCoHJ9D9Sy3TLp0mUHBrj1cg' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.webm = '"_"' //ganti pakai Instagram mu
+global.webm = 'https://www.instagram.com/danibotz_official' //ganti pakai Instagram mu
 global.saweria = '"_"' //bebas ganti aja pakai apa
 global.nomer = 'https://wa.me/6281251549899' //ganti jadi nomor mu
 global.price1 = '𝐍𝐈𝐇 𝐁𝐀𝐍𝐆 𝐋𝐈𝐒𝐓 𝐒𝐄𝐖𝐀𝐍𝐘𝐀\n\n》PERMANEN Rp 10.000\nMAKSIMAL 2 GRUP\n》PERMANEN  Rp 20.000\nMAKSIMAL 6 GRUP \n》1 BULAN Rp 5.000\nMAKSIMAL 1 GRUP'
